520d3d449a6428b3217882e649c98880b98d3779
Zeriab Chiah <zeriab@hotmail.com>
2022-05-05 01:45:58 +0300

    improved search

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,10 +6,72 @@ function Search({ details }) {
   const filteredbreweries = details.filter(
     brewerie => {
       return (
+       
         brewerie
           .name
           .toLowerCase()
           .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .brewery_type
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .city
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .id
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .state
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .postal_code
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+          brewerie
+          .country
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+          ||
+         (brewerie.county_province!=null)
+         &&
+         brewerie
+         .county_province
+         .toLowerCase()
+         .includes(searchField.toLowerCase())
+         ||
+         (brewerie.website_url!=null)
+         &&
+         brewerie
+         .website_url
+         .toLowerCase()
+         .includes(searchField.toLowerCase())
+         ||
+         brewerie
+         .created_at
+         .toLowerCase()
+         .includes(searchField.toLowerCase())
+         ||
+         brewerie
+         .updated_at
+         .toLowerCase()
+         .includes(searchField.toLowerCase())
+         ||
+         (brewerie.phone!=null)
+         &&
+         brewerie
+         .phone
+         .toLowerCase()
+         .includes(searchField.toLowerCase())
       );
     }
   );
@@ -21,20 +83,23 @@ function Search({ details }) {
 
 
   return (
-
+<center>
     <section className="garamond">
       <div className="navy georgia ma0 grow">
-        <h2 className="f2">Search your brewerie</h2>
+        <h2 className="f2">Search your brewery</h2>
       </div>
       <div className="pa2">
         <TextField id="standard-basic" label="Search" variant="standard" type="search"
-          placeholder="Search filteredbreweries"
+          placeholder="Search breweries"
           onChange={handleChange} />
       </div>
-      <br />  <br />  <br />
-      <SearchList breweries={filteredbreweries} />
+    <div>
+    <SearchList breweries={filteredbreweries} />
+    </div>
+     
 
     </section>
+    </center>
   );
 }
 
