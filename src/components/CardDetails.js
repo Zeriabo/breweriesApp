574a140421ce73786f32f3f99e6aa070b7b2e66c
Zeriab Chiah <zeriab@hotmail.com>
2022-05-06 18:02:47 +0300

    detail page

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -21,35 +21,35 @@ function CardDetails() {
 
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            <b>Name:</b> {(location.state.brewerie.name ? location.state.brewerie.name : "null")}
+            <b>Name:</b> {(location.state.brewerie.name!=null) ? location.state.brewerie.name : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>brewery_type:</b> {(location.state.brewerie.brewery_type) ? location.state.brewerie.brewery_type : "null"}
+            <b>brewery_type:</b> {(location.state.brewerie.brewery_type!=null) ? location.state.brewerie.brewery_type : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>city:</b> {(location.state.brewerie.city ? location.state.brewerie.city : "null")}
+            <b>city:</b> {(location.state.brewerie.city!=null) ? location.state.brewerie.city : "null"}
           </Typography>
 
           <Typography variant="body2" color="text.secondary">
-            <b>street:</b> {(location.state.brewerie.street ? location.state.brewerie.street : "null")}
+            <b>street:</b> {(location.state.brewerie.street!=null) ? location.state.brewerie.street : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>address_2:</b> {(location.state.brewerie.address_2 ? location.state.brewerie.address_2 : "null")}
+            <b>address_2:</b> {(location.state.brewerie.address_2!=null) ? location.state.brewerie.address_2 : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>address_3:</b> {(location.state.brewerie.address_3 ? location.state.brewerie.address_3 : "null")}
+            <b>address_3:</b> {(location.state.brewerie.address_3 !=null)? location.state.brewerie.address_3 : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>city:</b> {(location.state.brewerie.city ? location.state.brewerie.city : "null")}
+            <b>city:</b> {(location.state.brewerie.city!=null) ? location.state.brewerie.city : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>state:</b> {(location.state.brewerie.state ? location.state.brewerie.state : "null")}
+            <b>state:</b> {(location.state.brewerie.state !=null )? location.state.brewerie.state : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>county_province:</b> {(location.state.brewerie.county_province ? location.state.brewerie.county_province : "null")}
+            <b>county_province:</b> {(location.state.brewerie.county_province!=null) ? location.state.brewerie.county_province : "null"}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <b>postal_code:</b> {(location.state.brewerie.postal_code ? location.state.brewerie.postal_code : "null")}
+            <b>postal_code:</b> {(location.state.brewerie.postal_code !=null)? location.state.brewerie.postal_code : "null"}
           </Typography>
 
         </CardContent>
